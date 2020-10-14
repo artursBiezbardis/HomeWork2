4d612e3971f6e38a42b2e5a5316b1617e20157ac
artursBiezbardis <arturste2007@gmail.com>
2020-10-14 16:35:09 +0300

    Pin Entry

---
+++ PIN ENTRY/PinSearch.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+
+class PinSearch
+{
+    private $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    public function readCsvAllAsArrays(): array
+    {
+        $fp = fopen($this->path, "r");
+        $csvArray = [];
+
+        while ($row = fgetcsv($fp)) {
+            $csvArray[] = $row;
+        }
+        return $csvArray;
+
+    }
+    public function searchPinInList(array $allFromSavedSessionReferencesCsw, string $pinEntry): array
+    {
+        $person = '';
+        foreach ($allFromSavedSessionReferencesCsw as $sessionArray) {
+            if ($sessionArray[0] == $pinEntry && !empty($allFromSavedSessionReferencesCsw)) {
+                $person = $sessionArray;
+            }
+        }
+        if (!empty($person)) {
+            return $person;
+        } else {
+            return ['Pin kods nepareizs!'];
+        }
+    }
+}
